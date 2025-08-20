c7b17387781b3a4d1b38f7e5e41ac3bbed64d57e
Brendan Dileo <dileob23@gmail.com>
2025-08-20 18:36:57 -0400

    Update main

--- src/main/java/brendanddev/Main.java
+++ src/main/java/brendanddev/Main.java
@@ -1,2 +1,63 @@
 package brendanddev;
 
+/**
+ * Demonstrates the SimulatedHeap 
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        // Created a SimulatedHeap with 32 bytes of memory
+        SimulatedHeap heap = new SimulatedHeap(32);
+        
+        // Allocates 8 bytes of memory (first block)
+        int a = heap.malloc(8);
+
+        // Allocates another 8 bytes of memory (second block)
+        int b = heap.malloc(8);
+
+        // At this point:
+        // [0-7: allocated] [8-15: allocated] [16-31: free]
+        heap.printHeap();
+
+        // Free the first 8 byte block
+        heap.free(a);
+
+        // Now:
+        // [0-7: free] [8-15: allocated] [16-31: free]
+        // Fragmentation occurs, two free blocks are separated by an allocated block
+        heap.printHeap();
+
+        // Allocate another 4 bytes of memory
+        int c = heap.malloc(4);
+
+        // The allocator will put this 4 byte block inside the first free region (0-7),
+        // splitting it into [0-3: allocated] [4-7: free]
+        // Current state:
+        // [0-3: allocated] [4-7: free] [8-15: allocated] [16-31: free]
+        heap.printHeap();
+
+        // Free the second 8 byte block
+        heap.free(b);
+
+        // Now:
+        // [0-3: allocated] [4-7: free] [8-15: free] [16-31: free]
+        // Last three blocks will be merged into one big free block (coalescing)
+        heap.printHeap();
+
+        // Free the 4 byte block at the beginning
+        heap.free(c);
+
+        // Now:
+        // [0-3: free] [4-7: free] [8-15: free] [16-31: free]
+        // Entire heap is free again
+        heap.printHeap();
+        
+
+
+    }
+
+
+
+}
