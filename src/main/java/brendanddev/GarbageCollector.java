d8ed1d26b50ced15c9b69ee54e2ec4cca4b06368
Brendan Dileo <dileob23@gmail.com>
2025-08-21 17:00:48 -0400

    Update GC class

--- src/main/java/brendanddev/GarbageCollector.java
+++ src/main/java/brendanddev/GarbageCollector.java
@@ -1,5 +1,22 @@
 package brendanddev;
 
+/**
+ * A simulated GarbageCollector that manages memory in a simulated heap.
+ */
 public class GarbageCollector {
+
+    private final SimulatedHeap heap;
+    private final RootSet rootSet;
+
+    /**
+     * Constructs an instance of GarbageCollector.
+     * 
+     * @param heap The simulated heap to manage.
+     * @param rootSet The root set containing references to live objects.
+     */
+    public GarbageCollector(SimulatedHeap heap, RootSet rootSet) {
+        this.heap = heap;
+        this.rootSet = rootSet;
+    }
     
 }
